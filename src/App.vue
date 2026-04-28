c5d6710531df603f25ae01e598a6ff6041526f9d
nidulaj <nidulajayalath@gmail.com>
2026-04-28 09:15:51 +0530

    refactor: Simplify App.vue by removing NavBar and HomeView components

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,9 @@
 <script setup lang="ts">
-import NavBar from './components/NavBar.vue'
-
-import HomeView from './views/HomeView.vue';
 </script>
 
 <template>
-  <div class="bg-black">
-    <NavBar />
-    <HomeView />
+  <div>
+    <router-view />
   </div>
 </template>
    
